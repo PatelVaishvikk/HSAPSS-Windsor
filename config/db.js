8b4db468a161a5ce131b3301bc8d5a15d432e4cb
PatelVaishvik <vaishvikpatel001@gmail.com>
2025-01-15 00:20:46 -0500

    Initial commit with .gitignore

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'HSAPSS_Windsor',
-  password: 'dasnadas',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const studentRoutes = require('./routes/StudentRoutes');
+require('dotenv').config();
 
 const app = express();
 
